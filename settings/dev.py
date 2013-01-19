31ffb8ff7137235160e2a076780b761d065469c8
Rodrigo Daunoravicius <rodelrod@putoperdiz>
2013-01-19 12:45:01 +0100

    Added better error messages for templates. (I don't think this actually does anything, because we're using jinja.)

--- settings/dev.py
+++ settings/dev.py
@@ -3,3 +3,4 @@ DEBUG=True
 if 'test' in sys.argv:
     SOUTH_TESTS_MIGRATE=False
     DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}
+    TEMPLATE_STRING_IF_INVALID = 'INVALID EXPRESSION: %s'
