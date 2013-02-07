cd3e5fb1fa14623f59916e232653e6c8a9fb1488
Rodrigo Daunoravicius <rodelrod@putoperdiz>
2013-02-07 19:46:26 +0100

    Added some development requirements for testing and documentation.

--- settings/dev.py
+++ settings/dev.py
@@ -1,5 +1,9 @@
 from base import *
 DEBUG=True
+INSTALLED_APPS += (
+        'django_extensions',
+        'lettuce.django',
+        )
 if 'test' in sys.argv:
     SOUTH_TESTS_MIGRATE=False
     DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}
